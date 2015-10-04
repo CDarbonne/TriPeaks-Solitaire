ec568633f55b9af7937bfe5fe60265f5aad89bde
user <msu-jrobicheaux@student.mcneese.edu>
2015-10-04 12:30:59 -0500

    recognizes a win and a gameover

--- tripeaks_barebones_working/js/gameRules.js
+++ tripeaks_barebones_working/js/gameRules.js
@@ -14,96 +14,24 @@ function generalFunction(functionNumber, e) {
         break;
     }
 
-//  flipLeafNodes();
+  flipLeafNodes();
 
-/*
-  if(winCheck()) console.log("GAME WON");
-  else {
-    if(gameOverCheck()) console.log("GAME OVER");
-    else console.log("NOT GAME OVER");
-  }
-*/
-
-}
-
-
-function trashCards() {
-  /* NOT REALLY NEEDED */
-  console.log('Im a trash card');
-}
-
-function replaceFunctionNumber(currentElement, number) {
-  var searchString1 = 'generalFunction(';
-  var parameterIndexStart = currentElement.indexOf('generalFunction(');
-
-  parameterIndexStart = parameterIndexStart+searchString1.length;
-
-  var parameterIndexEnd = currentElement.indexOf(',', parameterIndexStart);
-  var newString = currentElement.substring(0, parameterIndexStart);
-
-  newString += number;
-  newString += currentElement.substring(parameterIndexEnd, currentElement.length);  
-
-  return newString;
-}
-
-
-function moveCardElementToTrash(domElement) {
-  var card = domElement;
-  var cardParentId = card.parentNode.id;
-  var originPile = document.getElementById(cardParentId);
-  var trashPile = document.getElementById('trashCards');
-
-  // create a copy of card (so that we can delete the old one)
-  var newCard = document.createElement('div');
-  newCard = card.outerHTML;
-  newCard = replaceFunctionNumber(newCard, 1);
-
-  // remove the card from its original pile
-  card.remove();
-
-  // set the trash pile to the card being thrown away
-  trashPile.innerHTML = newCard;
-}
-
-function shoeCards(e) {
-  var cardObject = UNUSED_DECK[UNUSED_DECK.length-1];
-  var cardDomElement = document.getElementById(''+cardObject.suit+cardObject.num);
-
-  moveCardOverTime(cardObject, USED_INIT_X, USED_INIT_Y, MOVE_TIME);
-  flipCardOverTime(cardObject, FLIP_TIME);
-
-  // move the dom element to the trash dom element (trashCards)
   setTimeout(function() {
-    moveCardElementToTrash(cardDomElement);
-    USED_DECK.push(UNUSED_DECK.pop());
-  }, MOVE_TIME);
-}
+    if(winCheck()) {
 
-function checkIfFieldCardCanBeThrown(cardObject) {
-  // check if it is a leaf node and face up
-  if(cardObject.isFaceUp && (cardObject.leftChild == null
-       && cardObject.rightChild == null)) {
+      /* PUT ON WIN CODE HERE */
 
-    // check if it is within range of trash card value
-    var trashCardNumber = USED_DECK[USED_DECK.length-1].num;
-    var lesser = trashCardNumber - 1;
-    if(lesser < 1) lesser = 13;
-    var greater = trashCardNumber + 1;
-    if(greater > 13) greater = 1;
-    
-    // if it is within range cut ties from parent nodes
-    if(cardObject.num == lesser || cardObject.num == greater) {
-      var cIndex = 0;
-      for(i = 0; i < TRIPEAKS_DECK.length; i++) {
-        var currentCard = TRIPEAKS_DECK[i];
-        if(currentCard.leftChild == cardObject.id) currentCard.leftChild = null;
-        if(currentCard.rightChild == cardObject.id) currentCard.rightChild = null;
+      console.log("GAME WON");
+    } else {
+      if(gameOverCheck()) {
+
+        /* PUT ON GAME OVER CODE HERE */
+
+        console.log("GAME OVER");
       }
-      return true;
     }
-  }
-  return false;
+  }, MOVE_TIME + 200);
+
 }
 
 function fieldCards(cardDomElement) {
@@ -143,6 +71,83 @@ function fieldCards(cardDomElement) {
   }
 }
 
+/* NOT REALLY NEEDED */
+function trashCards() {
+}
+
+function shoeCards(e) {
+  var cardObject = UNUSED_DECK[UNUSED_DECK.length-1];
+  var cardDomElement = document.getElementById(''+cardObject.suit+cardObject.num);
+
+  moveCardOverTime(cardObject, USED_INIT_X, USED_INIT_Y, MOVE_TIME);
+  flipCardOverTime(cardObject, FLIP_TIME);
+
+  // move the dom element to the trash dom element (trashCards)
+  setTimeout(function() {
+    moveCardElementToTrash(cardDomElement);
+    USED_DECK.push(UNUSED_DECK.pop());
+  }, MOVE_TIME);
+}
+
+function replaceFunctionNumber(currentElement, number) {
+  var searchString1 = 'generalFunction(';
+  var parameterIndexStart = currentElement.indexOf('generalFunction(');
+
+  parameterIndexStart = parameterIndexStart+searchString1.length;
+
+  var parameterIndexEnd = currentElement.indexOf(',', parameterIndexStart);
+  var newString = currentElement.substring(0, parameterIndexStart);
+
+  newString += number;
+  newString += currentElement.substring(parameterIndexEnd, currentElement.length);  
+
+  return newString;
+}
+
+function moveCardElementToTrash(domElement) {
+  var card = domElement;
+  var cardParentId = card.parentNode.id;
+  var originPile = document.getElementById(cardParentId);
+  var trashPile = document.getElementById('trashCards');
+
+  // create a copy of card (so that we can delete the old one)
+  var newCard = document.createElement('div');
+  newCard = card.outerHTML;
+  newCard = replaceFunctionNumber(newCard, 1);
+
+  // remove the card from its original pile
+  card.remove();
+
+  // set the trash pile to the card being thrown away
+  trashPile.innerHTML = newCard;
+}
+
+function checkIfFieldCardCanBeThrown(cardObject) {
+  // check if it is a leaf node and face up
+  if(cardObject.isFaceUp && (cardObject.leftChild == null
+       && cardObject.rightChild == null)) {
+
+    // check if it is within range of trash card value
+    var trashCardNumber = USED_DECK[USED_DECK.length-1].num;
+    var lesser = trashCardNumber - 1;
+    if(lesser < 1) lesser = 13;
+    var greater = trashCardNumber + 1;
+    if(greater > 13) greater = 1;
+    
+    // if it is within range cut ties from parent nodes
+    if(cardObject.num == lesser || cardObject.num == greater) {
+      var cIndex = 0;
+      for(i = 0; i < TRIPEAKS_DECK.length; i++) {
+        var currentCard = TRIPEAKS_DECK[i];
+        if(currentCard.leftChild == cardObject.id) currentCard.leftChild = null;
+        if(currentCard.rightChild == cardObject.id) currentCard.rightChild = null;
+      }
+      return true;
+    }
+  }
+  return false;
+}
+
 function flipLeafNodes() {
   TRIPEAKS_DECK.forEach(function(e) {
     if(e.leftChild == null && e.rightChild == null && !e.isFaceUp) {
@@ -169,25 +174,9 @@ function moveCardToTrash(domElement) {
   trashPile.innerHTML = newCard;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function winCheck() {
+  return (TRIPEAKS_DECK.length == 0);
+}
 
 function gameOverCheck() {
   var i;
@@ -219,39 +208,3 @@ function gameOverCheck() {
 
   return gameOver;
 }
-
-function winCheck() {
-  return (TRIPEAKS_DECK.length == 0);
-}
-
-
-/*
-// BACKUP OF MOVE TO TRASH
-
-function moveToTrash(id) {
-    var shoeCardsHTML = document.getElementById(id);
-    shoeCardsHTML = shoeCardsHTML.getElementsByClassName('moveContainer');
-
-    var trashCardsHTML = document.getElementById('trashCards');
-
-    var currentElement = document.createElement('div');
-    currentElement = shoeCardsHTML[shoeCardsHTML.length-1].outerHTML;
-    currentElement = replaceFunctionNumber(currentElement, 1);
-
-    shoeCardsHTML[shoeCardsHTML.length-1].remove();
-
-    trashCardsHTML.innerHTML = currentElement;
-}
-
-function moveFieldCardToTrash(id, cardId) {
-    // id = fieldCards
-    var fieldCardsHTML = document.getElementById(cardId);
-    var trashCardsHTML = document.getElementById('trashCards');
-
-    var currentElement = document.createElement('div');
-    currentElement = fieldCardsHTML.outerHTML;
-
-    fieldCardsHTML.remove();
-    trashCardsHTML.innerHTML = currentElement;
-}
-*/
